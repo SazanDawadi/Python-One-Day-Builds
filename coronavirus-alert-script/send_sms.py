652afca215a8f1416e96c8c3af720d40d6fcdf0b
Sajan Dawadi <68532008+SazanDawadi@users.noreply.github.com>
2020-08-04 21:17:38 +0545

    comments added on send_sms.py

--- coronavirus-alert-script/send_sms.py
+++ coronavirus-alert-script/send_sms.py
@@ -1,5 +1,8 @@
 from twilio.rest import Client
 from credentials import account_sid, auth_token, my_cell, my_twilio
+# make a new python file and add variable account_sid, auth_token, my_cell, my_twilio
+# assign it value and import it in this file
+# you can get your api key and auth token from twilio website
 
 def send_sms(msg):
     client = Client(account_sid, auth_token)  
